3a432fb0bc38fbd94f111676630769495b533242
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-07-07 10:11:04 +0500

    Add TypeScript files with string manipulation examples

---
+++ TypeScript/log.js
@@ -0,0 +1,21 @@
+var text = " TypeScript is Awesome ";
+// text.length;                  // ➤ Get length
+// text.trim();                  // ➤ Remove spaces
+// text.toLowerCase();           // ➤ Lowercase
+// text.toUpperCase();           // ➤ Uppercase
+// text.includes("Awesome");     // ➤ Check if contains
+// text.indexOf("is");           // ➤ Find index
+// text.replace("Awesome", "Cool"); // ➤ Replace text
+// text.split(" ");              // ➤ Split into array
+// text.slice(0, 10);            // ➤ Cut part
+// text.startsWith("Type");      // ➤ Starts with
+console.log(text.length);
+console.log(text.trim());
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.includes("Awesome"));
+console.log(text.indexOf("is"));
+console.log(text.replace("Awesome", "Cool"));
+console.log(text.split(" "));
+console.log(text.slice(0, 10));
+console.log(text.startsWith(" Type"));
---
+++ TypeScript/str.js
@@ -0,0 +1,11 @@
+var text = " TypeScript is Awesome ";
+text.length; // ➤ Get length
+text.trim(); // ➤ Remove spaces
+text.toLowerCase(); // ➤ Lowercase
+text.toUpperCase(); // ➤ Uppercase
+text.includes("Awesome"); // ➤ Check if contains
+text.indexOf("is"); // ➤ Find index
+text.replace("Awesome", "Cool"); // ➤ Replace text
+text.split(" "); // ➤ Split into array
+text.slice(0, 10); // ➤ Cut part
+text.startsWith("Type"); // ➤ Starts with
