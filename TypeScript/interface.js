2ecf1557f8320e2ac8427259c52f535391a66a25
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-07-07 10:34:32 +0500

    Add User interface and user1 object in TypeScript

---
+++ TypeScript/interface.js
@@ -0,0 +1,5 @@
+var user1 = {
+    username: "AbrorBro",
+    email: "siddiqjonovsaidabror@gmail.com",
+    isAdmin: true,
+};
---
+++ TypeScript/interface.ts
@@ -0,0 +1,11 @@
+interface User{
+    username: string;
+    email: string;
+    isAdmin?: boolean;
+}
+
+const user1: User = {
+    username: "AbrorBro",
+    email: "siddiqjonovsaidabror@gmail.com",
+    isAdmin: true,
+}
